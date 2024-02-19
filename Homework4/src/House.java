5341e20fb13e6dc8b84ce51cb49f2c8e21e5bef5
s4mmie <terrordbz@gmail.com>
2024-02-19 10:57:34 -0500

    homework4 update

--- Homework4/src/House.java
+++ Homework4/src/House.java
@@ -9,7 +9,6 @@
  * Changing those variables should change how the house is drawn. 
  */
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JFrame;
@@ -24,41 +23,43 @@ public class House extends JFrame {
 
 	public void paint(Graphics g)
 	{
-		final int houseX = 150;
+		final int houseX = 150; //house top left and right with how wide and tall
 		final int houseY = 200;
 		final int houseWidth = (int)(houseX * 2.5);
 		final int houseHeight = (int)(houseY * 2);
 		
-		final int win1X = houseX+50;
+		final int win1X = houseX+50; //window 1& 2 X+Y and the width and height
 		final int win1Y = houseY+60;
 		final int win2X = houseX+250;
 		final int win2Y = houseY+60;
 		final int winWidth = houseX/2;
 		final int winHeight = houseY/2;
-		
+		//door width height x y
 		final int doorWidth = (int)(houseWidth/3);
 		final int doorX = houseX+(int)(houseWidth/2)-(int)(doorWidth/2);
 		final int doorY = houseY+houseHeight - (int)(houseHeight/2.5);
 		final int doorHeight = houseY+houseHeight-doorY;
-		
+		//roof and the points to draw the roof
 		final int roofHeight = houseY - (int)(houseHeight/3);
 		final int[] xPoints = { houseX, houseX + (int)(houseWidth/2), houseX+houseWidth};
 		final int[] yPoints = { houseY, roofHeight, houseY};
 		
 		g.setColor(Color.WHITE);
-		g.fillRect(0,0, getWidth(), getHeight());
+		g.fillRect(0,0, getWidth(), getHeight());//draw blank canvas
 		
 		g.setColor(Color.GRAY);
-		g.fillRect(houseX, houseY, houseWidth, houseHeight);
+		g.fillRect(houseX, houseY, houseWidth, houseHeight);//draw house wall
 		
 		g.setColor(Color.RED);
-		g.fillPolygon(xPoints, yPoints, 3);
+		g.fillPolygon(xPoints, yPoints, 3);//draw house roof
 		
 		g.setColor(Color.BLACK);
-		g.drawRect(win1X, win1Y, winWidth, winHeight);
-		g.drawRect(win2X, win2Y, winWidth, winHeight);
+		g.drawRect(win1X, win1Y, winWidth, winHeight);//draw window 1
+		g.drawRect(win2X, win2Y, winWidth, winHeight); //draw window 2
 		
-		g.fillRect(doorX, doorY, doorWidth, doorHeight);
+		g.fillRect(doorX, doorY, doorWidth, doorHeight);//draw door
+		
+		g.drawOval(200,200,200,200);
 	}
 	
 	
--- Homework4/src/WordCheck.java
+++ Homework4/src/WordCheck.java
@@ -15,24 +15,24 @@ public class WordCheck {
 	{
 		System.out.println("This program will check to see if the first and last letter in your word are vowels.");
 		System.out.print("Please enter your word: ");
-		String word = scan.nextLine();
-		int wordLength = word.length();
-		boolean isWord = false;
-		if(word.indexOf(' ') != -1)
+		String word = scan.nextLine();//get word from user
+		int wordLength = word.length();//set the word elgnth to an int
+		boolean isWord = false;//see if the word is valid of starting and ending with AEIOU
+		if(word.indexOf(' ') != -1)//check to see if a space is in the word and exit if so
 		{
 			System.out.println("The word contains a space and is not a single word.");
 			System.exit(1);
 		}
-		
+		//check uppercase character at first index and see if it is AEIOU
 		if(Character.toUpperCase(word.charAt(0)) == 'A' || Character.toUpperCase(word.charAt(0)) == 'E' || Character.toUpperCase(word.charAt(0)) == 'I' || Character.toUpperCase(word.charAt(0)) == 'O' || Character.toUpperCase(word.charAt(0)) == 'U')
-		{
+		{//check to see uppercase character is at the last index and see if its AEIOU
 			if(Character.toUpperCase(word.charAt(wordLength-1)) == 'A' || Character.toUpperCase(word.charAt(wordLength-1)) == 'E' || Character.toUpperCase(word.charAt(wordLength-1)) == 'I' || Character.toUpperCase(word.charAt(wordLength-1)) == 'O' || Character.toUpperCase(word.charAt(wordLength-1)) == 'U')
 			{
 				System.out.printf("Yes the word '%s' starts and ends with a vowel!", word);
-				isWord = true;
+				isWord = true;//the word starts and ends with a vowel
 			}
 		}
-		if(!isWord)
+		if(!isWord)//the word doesnt start and end with a vowel
 		{
 			System.out.printf("No the word '%s' does not start and end with a vowel.", word);
 		}
